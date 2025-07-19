8803ae307f815a97e84ead4fca6ff3af01c4088e
Paeythor <theschumann@byui.edu>
2025-07-19 17:51:33 -0600

    UPDATES

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,16 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
+baseController.buildHome = async function(req, res, next) {
+  try {
+    const nav = await utilities.getNav()
+    res.render("index", {
+      title: "Home",
+      nav
+    })
+  } catch (error) {
+    next(error) // pass errors to Express error handler
+  }
 }
 
 module.exports = baseController
