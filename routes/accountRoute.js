0adcb31d7b262a728520049a3b3882f4769289e5
Paeythor <theschumann@byui.edu>
2025-08-16 11:50:27 -0600

    update

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = new express.Router()
+const router = express.Router()
 const accountController = require("../controllers/accountController")
 const regValidate = require("../utilities/account-validation")
 const utilities = require("../utilities")
@@ -30,64 +30,3 @@ router.post(
 router.get("/", utilities.handleErrors(accountController.buildAccount))
 
 module.exports = router
-async function buildLogin(req, res) {
-  let nav = await utilities.getNav()
-  res.render("account/login", {
-    title: "Login",
-    nav,
-    errors: null,
-    account_email: "",
-  })
-}
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-require("dotenv").config()
-
-async function accountLogin(req, res) {
-  let nav = await utilities.getNav()
-  const { account_email, account_password } = req.body
-  const accountData = await accountModel.getAccountByEmail(account_email)
-
-  if (!accountData) {
-    req.flash("notice", "Please check your credentials and try again.")
-    return res.status(400).render("account/login", {
-      title: "Login",
-      nav,
-      errors: null,
-      account_email,
-    })
-  }
-
-  try {
-    const passwordMatch = await bcrypt.compare(account_password, accountData.account_password)
-    if (passwordMatch) {
-      delete accountData.account_password
-
-      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: 3600, // 1 hour in seconds
-      })
-
-      const cookieOptions = {
-        httpOnly: true,
-        maxAge: 3600 * 1000, // 1 hour in milliseconds
-      }
-
-      if (process.env.NODE_ENV !== "development") {
-        cookieOptions.secure = true
-      }
-
-      res.cookie("jwt", accessToken, cookieOptions)
-      return res.redirect("/account/")
-    } else {
-      req.flash("notice", "Please check your credentials and try again.")
-      return res.status(400).render("account/login", {
-        title: "Login",
-        nav,
-        errors: null,
-        account_email,
-      })
-    }
-  } catch (error) {
-    return res.status(500).send("Server Error")
-  }
-}
